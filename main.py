2980b9421c1ab6e1eb711d381c8638f3eb146442
aleksbrsc <aleksbrsc@gmail.com>
2023-05-12 01:36:37 -0400

    Main

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-# Basic moneyline bet arbitrage finder program 
+# Basic moneyline arbitrage bet finder program 
 # calculates the discrepancies in odds across different bookmakers provided by user input and returns the arbitrage opportunities
+# for more info on arbitrage betting: https://en.wikipedia.org/wiki/Arbitrage_betting
+
+
